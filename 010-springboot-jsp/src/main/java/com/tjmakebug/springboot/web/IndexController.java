c58cb183a88d082d2c669123d58a4eab8c9436ee
TED10240 <classic10240@gmail.com>
2022-03-29 00:00:28 +0800

    SpringBoot集成jsp

---
+++ 010-springboot-jsp/src/main/java/com/tjmakebug/springboot/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.tjmakebug.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping(value = "/say")
+    public ModelAndView  say(){
+        ModelAndView mv =  new ModelAndView();
+        mv.addObject("message","Hello");
+        mv.setViewName("index");
+        return mv;
+    }
+
+    @RequestMapping(value = "index")
+    public String index(Model model){
+        model.addAttribute("message","Hello Spring");
+        return "index";
+    }
+}
---
+++ 010-springboot-jsp/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: class
+  Date: 2022/3/28
+  Time: 23:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
